dd0772393d657958f983da06abb8e15a772319e9
deepakjoshi <djoshi@ekline.io>
2024-04-28 15:54:44 +0530

    Add : Product card complete

---
+++ src/app/(home)/components/DummyData/index.tsx
@@ -0,0 +1,46 @@
+import { Product } from "../types";
+export const dummyProducts: Product[] = [
+    {
+        id: '1',
+        name: 'Margherita',
+        description: 'Cheese and tomato',
+        price: 1000,
+        image: '/pizza-main.png'
+    },
+    {
+        id: '2',
+        name: 'Pepperoni',
+        description: 'Pepperoni and cheese',
+        price: 1200,
+        image: '/pizza-main.png'
+    },
+    {
+        id: '3',
+        name: 'Hawaiian',
+        description: 'Pineapple and ham',
+        price: 1500,
+        image: '/pizza-main.png'
+    },
+    {
+        id: '4',
+        name: 'Meat Feast',
+        description: 'Pepperoni, ham, and sausage',
+        price: 1800,
+        image: '/pizza-main.png'
+    },
+    {
+        id: '5',
+        name: 'Veggie Supreme',
+        description: 'Mushrooms, peppers, and onions',
+        price: 1600,
+        image: '/pizza-main.png'
+    },
+    {
+        id: '6',
+        name: 'Vegan',
+        description: 'Vegan cheese and tomato',
+        price: 1400,
+        image: '/pizza-main.png'
+    }
+];
+
